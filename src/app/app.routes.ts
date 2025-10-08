a1bc70e312d29320c2b1e2696f76052229a94805
NATHAN FERRAZ LIBERATTI <nathan.fliberatti@senacsp.edu.br>
2025-10-07 22:05:07 -0300

    Adicionado route pessoas/incluir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PessoaLogin } from './pages/pessoa-login/pessoa-login';
 import { PessoaListagem } from './pages/pessoa-listagem/pessoa-listagem';
+import { PessoaFormComponent } from './pages/pessoa-form/pessoa-form';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,11 @@ export const routes: Routes = [
         path: 'pessoas',
         component: PessoaListagem,
         title: 'Lista de pessoas'
+    },
+    {
+        path: 'pessoas/incluir',
+        component: PessoaFormComponent,
+        title: 'Cadastrar'
     }
 
 ];
