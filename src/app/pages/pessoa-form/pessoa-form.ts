cce7d64ecf33cf75979e315b2845e10e15625ecb
NATHAN FERRAZ LIBERATTI <nathan.fliberatti@senacsp.edu.br>
2025-10-07 21:55:15 -0300

    adicionado component pessoa-form

--- src/app/pages/pessoa-form/pessoa-form.ts
+++ src/app/pages/pessoa-form/pessoa-form.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pessoa-form',
+  standalone: true,
   imports: [],
   templateUrl: './pessoa-form.html',
   styleUrl: './pessoa-form.css'
 })
-export class PessoaForm {
+
+export class PessoaFormComponent {
+
+  titulo = 'Cadastro de Pessoas';
 
 }
